6ab7591ee52cd200d9002dde1235f59311cf041e
Ephy Kimotho <ephyk48@gmail.com>
2025-07-05 22:01:07 +0300

    updated Auth context and main app file

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,16 @@
+import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
-import { AuthProvider } from "./context/AuthContext";
-import { ToastProvider } from "./context/ToastContext";
-import './index.css';
+import AuthProvider from "./context/AuthContext";
+import SWRwithRouter from "./SWRwithRouter";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-      <ToastProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ToastProvider>
+      <BrowserRouter>
+        <SWRwithRouter />
+      </BrowserRouter>
     </AuthProvider>
   </React.StrictMode>
 );
