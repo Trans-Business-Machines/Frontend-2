8c900432e9a2126fba15d2c1170f9245c4dda1f5
Ephy Kimotho <ephyk48@gmail.com>
2025-07-17 16:37:04 +0300

    used the custom Loader component

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,15 +1,14 @@
 /* eslint-disable react-refresh/only-export-components */
 /* eslint-disable no-unused-vars */
 import { createContext, useContext, useState, useEffect } from "react";
+import { jwtDecode } from "jwt-decode";
+import axios from "axios";
+import navigate from "../utils";
+import Loading from "../components/Loading";
 import axiosInstance, {
   baseURL,
   setAccessToken as setAxiosToken,
 } from "../api/axiosInstance";
-import axios from "axios";
-
-import { jwtDecode } from "jwt-decode";
-
-import navigate from "../utils";
 
 const AuthContext = createContext();
 
@@ -49,44 +48,44 @@ export function AuthProvider({ children }) {
 
   const login = async (creds) => {
     // call the login endpoint
-    // if successfull then
-    // set the access token to the axios instance and the state
-    // decode the token to get the user details
-    // send back the decoded user details
-    // else throw an error
     try {
+      // if successfull then
       const { data } = await axios.post(`${baseURL}/auth/login`, creds, {
         withCredentials: true, // to allow cookies to be sent with the request
       });
+      // set the access token to the axios instance and the state
       setAxiosToken(data.accessToken);
       setToken(data.accessToken);
 
-      // Decode the access token to get user details
+      // decode the token to get the user details
       const decodedUser = jwtDecode(data.accessToken);
+      //  save the user in context
       setUser(decodedUser);
+      // send back the decoded user details
       return decodedUser;
     } catch (error) {
-      console.error("Login failed:", error);
-      throw error; // rethrow the error to be handled by the caller
+      // else throw an error
+      throw error;
     }
   };
 
   const logout = async () => {
     // call the logout endpoint
-    // clear the access token from the axios instance
-    // clear the user state
     try {
       await axiosInstance.post("/auth/logout");
+      // clear the access token from the axios instance
       setAxiosToken(null);
+      // clear the user state
       setUser(null);
     } catch (error) {
+      // throw error if logout failed
       console.error("Logout failed:", error);
     }
   };
 
   return (
     <AuthContext.Provider value={{ user, token, login, logout, loading }}>
-      {loading ? <div>Verifying user info...</div> : children}
+      {loading ? <Loading /> : children}
     </AuthContext.Provider>
   );
 }
